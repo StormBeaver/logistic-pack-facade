01ed08e163a73a288a5f4764340985a9ad7bd018
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-09-19 15:03:25 +0300

    unite subscribe and start_consume to one file

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -2,10 +2,13 @@ package consumer
 
 import (
 	"context"
+	"log"
 	"sync"
+	"time"
 
 	"github.com/IBM/sarama"
 	"github.com/stormbeaver/logistic-kw-pack-facade/internal/config"
+	"github.com/stormbeaver/logistic-kw-pack-facade/internal/model"
 )
 
 func StartConsuming(ctx context.Context, cfg *config.Kafka, wg *sync.WaitGroup) error {
@@ -23,5 +26,34 @@ func StartConsuming(ctx context.Context, cfg *config.Kafka, wg *sync.WaitGroup) 
 		subscribe(ctx, topic, consumerGroup, cfg.Tick, wg)
 	}
 
+	return nil
+}
+
+func subscribe(
+	ctx context.Context,
+	topic string,
+	group sarama.ConsumerGroup,
+	consumeRate time.Duration,
+	wg *sync.WaitGroup,
+) error {
+	ticker := time.NewTicker(consumeRate)
+	consumer := model.Consumer{}
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				if err := group.Consume(ctx, []string{topic}, &consumer); err != nil {
+					log.Fatal(err)
+				}
+				if ctx.Err() != nil {
+					log.Fatal(ctx.Err())
+				}
+			case <-ctx.Done():
+				wg.Done()
+				return
+			}
+		}
+	}()
 	return nil
 }
